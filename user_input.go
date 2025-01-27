7e34c933e448076a6be532864e946581bce47920
saifu9421 <saifursaif905@gmail.com>
2025-01-27 12:30:14 +0600

    user-input

---
+++ user_input.go
@@ -0,0 +1,24 @@
+package main 
+import "fmt";
+ func main(){
+	//  var name string;
+	//  fmt.Printf("your Name Input = ");
+	//   fmt.Scan(&name);
+	//    fmt.Println("Your name = ", name);
+	//  var name string;
+	//   var  age int;
+	//    fmt.Println("Enter Your Name = ");
+	//     fmt.Scan(&name);
+	// 	 fmt.Println("Enter Your Age = ");
+	// 	fmt.Scan(&age);
+	// 	fmt.Println("name = ",name, "age = ",age);  
+	 var name string;
+	  var age  int;
+	 fmt.Println("Your name and age = ");
+	 fmt.Scanln(&name,&age);
+	//   fmt.Scanln(&age);
+	   fmt.Println("Your name adn age = ",name,age);
+	   var rn float64;
+	    fmt.Scanln(&rn);
+		 println(rn);
+ }
