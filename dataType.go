c972201a4050e12e1fc44846f7e7bac8897afc45
saifu9421 <saifursaif905@gmail.com>
2025-01-27 10:05:31 +0600

    data-type

---
+++ dataType.go
@@ -0,0 +1,17 @@
+package main 
+import "fmt";
+ func main(){
+	// var  x int ;
+	 x := 100;
+	   fmt.Println("value is x = ",x);
+	   var nam string;
+	    nam = "saifur rahman saif";
+		 fmt.Println("my name = ",nam);
+		 name := "jhon";
+		 fmt.Println("name = ",name);
+		 var random float64;
+		 random = 2.34;
+		  fmt.Println(random);
+		  ran := 3.45;
+		   fmt.Println(ran);
+ };
